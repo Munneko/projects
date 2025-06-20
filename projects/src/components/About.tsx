62fdc14da3a87b6c31d3c27a6c148b032bd76b98
Diana <munneko13@gmail.com>
2025-06-20 20:35:42 +0300

    About created

---
+++ projects/src/components/About.tsx
@@ -0,0 +1,63 @@
+import "../styles/about.scss";
+const About = () => {
+  return (
+    <section className="section about">
+      <div className="container">
+        <h2 className="section-title">Lorem ipsum</h2>
+        <div className="about__content">
+          <div className="about__text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed
+              finibus augue. Aliquam urna turpis, dapibus vel dolor sit amet,
+              convallis placerat odio. Donec hendrerit, neque ut sollicitudin
+              porta, erat metus iaculis nibh, ut blandit ante massa sed diam.
+              Vestibulum at orci eleifend, elementum tellus rutrum, mattis est.
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas. Aliquam tincidunt purus at erat
+              commodo ullamcorper. Etiam sagittis pretium est, a ultricies velit
+              tristique et. Aenean porttitor augue vitae lacinia lacinia.
+              Vestibulum convallis pretium mi, eget varius nulla laoreet vitae.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed
+              finibus augue. Aliquam urna turpis, dapibus vel dolor sit amet,
+              convallis placerat odio. Donec hendrerit, neque ut sollicitudin
+              porta, erat metus iaculis nibh, ut blandit ante massa sed diam.
+              Vestibulum at orci eleifend, elementum tellus rutrum, mattis est.
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas. Aliquam tincidunt purus at erat
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed
+              finibus augue. Aliquam urna turpis, dapibus vel dolor sit amet,
+              convallis placerat odio. Donec hendrerit, neque ut sollicitudin
+              porta, erat metus iaculis nibh, ut blandit ante massa sed diam.
+              Vestibulum at orci eleifend, elementum tellus rutrum, mattis est.
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas. Aliquam tincidunt purus at erat
+              commodo ullamcorper. Etiam sagittis pretium est, a ultricies velit
+              tristique et. Aenean porttitor augue vitae lacinia lacinia.
+              Vestibulum convallis pretium mi, eget varius nulla laoreet vitae.
+            </p>
+          </div>
+          <div className="about__stats">
+            <div className="about__stat">
+              <h3>Lorem</h3>
+              <p>Lorem ipsum</p>
+            </div>
+            <div className="about__stat">
+              <h3>Lorem</h3>
+              <p>Lorem ipsum</p>
+            </div>
+            <div className="about__stat">
+              <h3>Lorem</h3>
+              <p>Lorem ipsum</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- projects/src/pages/Home.tsx
+++ projects/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
+import About from "../components/About";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Hero />
       <Skills />
+      <About />
     </div>
   );
 };
